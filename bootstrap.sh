11536b8135bbdb80fbf965ba989b8710216714f2
Ryan Coppolo <r@coppolo.com>
2014-04-06 14:45:30 -0700

    Try at automating.

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DOTFILES_ROOT="`pwd`"
+
+set -e
+
+function link_to_home {
+  test -L "$HOME/.$1" || ln -s "$DOTFILES_ROOT/$1" "$HOME/.$1"
+}
+
+mkdir -p ~/.vim/{bundle,colors,syntax}
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
+
+link_to_home "vimrc"
+link_to_home "gitconfig"
+link_to_home "tmux.conf"
+
