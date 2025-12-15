389e16bcc7d5b8f47d516591a3df21bde99f07ef
Ryan Coppolo <ryan@rockfruit.com>
2025-12-15 08:46:15 -0500

    Update script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,34 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-DOTFILES_ROOT="`pwd`"
+DOTFILES_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-set -e
+link_file() {
+  local src="$DOTFILES_ROOT/$1"
+  local dst="$HOME/.$1"
 
-function link_to_home {
-  test -L "$HOME/.$1" || ln -s "$DOTFILES_ROOT/$1" "$HOME/.$1"
+  if [ -L "$dst" ]; then
+    echo "Skipping $dst (symlink exists)"
+  elif [ -e "$dst" ]; then
+    echo "Backing up $dst to $dst.backup"
+    mv "$dst" "$dst.backup"
+    ln -s "$src" "$dst"
+    echo "Linked $dst"
+  else
+    ln -s "$src" "$dst"
+    echo "Linked $dst"
+  fi
 }
 
-mkdir -p ~/.vim/{bundle,colors,syntax}
-
-if [ ! -d "$HOME/.vim/bundle/vundle" ]; then
-  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
+# vim-plug (matches what vimrc uses)
+if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
+  echo "Installing vim-plug..."
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-link_to_home "vimrc"
-link_to_home "gitconfig"
-link_to_home "tmux.conf"
+link_file "vimrc"
+link_file "gitconfig"
+link_file "tmux.conf"
+
+echo "Done! Run :PlugInstall in vim to install plugins."
